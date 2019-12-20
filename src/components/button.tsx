c8947143ac2c757b3c43327fb6e049dd1f4ebeb7
Jonas Munsch <jonas1.munsch@web.de>
2019-12-20 11:29:59 +0100

    add button styling and mock onclick

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledButton = styled.button`
-  background: purple;
-  border: white;
+  background: transparent;
+  border: 2px solid white;
   color: white;
+  margin: 5px;
+  padding: 20px;
+  width: 170px;
+  font-size: 20px;
+  outline: none;
+  :hover {
+    transform: scale(1.05);
+  }
 `;
 
 interface Button {
   text: string;
 }
 export const Button: React.FC<Button> = ({ text }) => {
-  return <StyledButton>{text}</StyledButton>;
+  return <StyledButton onClick={() => console.log(text)}>{text}</StyledButton>;
 };
