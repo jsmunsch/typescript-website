a1f33ca6abbc65deeb5a68188670d22797b3f5a1
Jonas Munsch <jonas1.munsch@web.de>
2019-12-29 21:44:38 +0100

    resize image responsive

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -6,6 +6,10 @@ const BackgroundImage = styled.div`
   width: 100vw;
   background: url(https://images.unsplash.com/photo-1468495244123-6c6c332eeece?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1522&q=80)
     no-repeat center center/150% fixed;
+  @media (min-width: 2000px) {
+    background: url(https://images.unsplash.com/photo-1468495244123-6c6c332eeece?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1522&q=80)
+      no-repeat center center/250% fixed;
+  }
 `;
 
 export const Background: React.FC = ({ children }) => {
